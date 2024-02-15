e148aafab305acb7c4c18aa84e51af0728ad7668
Parker <p.cronin.connolly@gmail.com>
2024-02-15 17:26:55 -0500

    saving before standdown

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const[plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(resp => resp.json())
+    .then(plantData => {
+      setPlants(plantData)
+  
+    })
+    .catch(error => {
+      console.log("Error with the fetch", error)
+    })
+  }, []);
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map(plant => (
+      <PlantCard 
+      key={plant.id}
+      plant={plant} />
+    ))}
+    </ul>
   );
 }
 
